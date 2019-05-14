57a520edcfa46fb101064f3e11a1b40c1c5f4a3c
StevieNG <sengfongng@gmail.com>
2019-05-14 22:28:21 +0100

    cleaned up codes

--- data.py
+++ data.py
@@ -1,11 +1,12 @@
 
-import csv , json, time
-from collections import OrderedDict
+import csv , json, time, collections 
 
-    # OrderDict for a sturture dict , otherwise showing as "to": then "from":
+#   OrderDict for a sturture dict , otherwise showing as "to": then "from":
+
+#   user input file name to be converted
 
 csvFilePath= input("Enter the CSV file you want to convert:(data.csv as demo)")
-#   raw_input for python2.7,  input if using python3+ 
+#   "raw_input" for python2.7, "input if" using python3+ 
 #   user input for file name it was originally hardcoded as: 
 #       csvFilePath = "data.csv" 
 
@@ -61,25 +62,25 @@ def csvObjectsToCollections(obj):
         
         # append 4-5 relationship to edges.. then 3-4, 2-3,etc
         if( items.get("Level4") and items.get("Level5")):
-            edge_4_to_5 = OrderedDict();            
+            edge_4_to_5 = {};            
             edge_4_to_5["from"] = items.get("Level4")
             edge_4_to_5["to"] = items.get("Level5")
             edges.append(edge_4_to_5)
 
         if( items.get("Level3") and items.get("Level4")):
-            edge_3_to_4 = OrderedDict();            
+            edge_3_to_4 = {};            
             edge_3_to_4["from"] = items.get("Level3")
             edge_3_to_4["to"] = items.get("Level4")
             edges.append(edge_3_to_4)
 
         if( items.get("Level2") and items.get("Level3")):
-            edge_2_to_3 = OrderedDict();            
+            edge_2_to_3 = {};            
             edge_2_to_3["from"] = items.get("Level2")
             edge_2_to_3["to"] = items.get("Level3")
             edges.append(edge_2_to_3)
 
         if( items.get("Level1") and items.get("Level2")):
-            edge_1_to_2 = OrderedDict();            
+            edge_1_to_2 = {};            
             edge_1_to_2["from"] = items.get("Level1")
             edge_1_to_2["to"] = items.get("Level2")
             edges.append(edge_1_to_2)
@@ -96,19 +97,17 @@ def csvObjectsToCollections(obj):
 
     # get current time for the file name
     currentTime= int(round(time.time(),0))
-    # print(currentTime)
-    
+   
     # create file name path
     nodesFilePath = "{}_nodes_collection.json".format(currentTime)
     edgesFilePath = "{}_edges_collection.json".format(currentTime)
 
     # write to a new json file with time stamp
-
     with open(nodesFilePath, "w") as jsonFile:
         jsonFile.write(json.dumps(nodes))
 
     with open(edgesFilePath, "w") as jsonFile:
         jsonFile.write(json.dumps(edges))
 
-# run funtion - * to be added to button on html
+# run funtion - ** to be added to button on html
 csvObjectsToCollections(arr)
